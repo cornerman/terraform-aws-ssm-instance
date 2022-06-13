31c4428bf234e6098ec565b2f8783d01fc2dfbc7
johannes karoff <johannes@karoff.net>
2022-06-13 13:28:34 +0200

    remove required provider

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "~> 4.17.0"
-      configuration_aliases = [aws]
+      source  = "hashicorp/aws"
+      version = "~> 4.17.0"
     }
   }
 }
