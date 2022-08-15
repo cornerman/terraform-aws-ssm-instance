764c6ce737184f449be2a6a58793f9447242d95c
johannes karoff <johannes@karoff.net>
2022-08-15 16:26:39 +0200

    formatting

--- instance.tf
+++ instance.tf
@@ -2,20 +2,6 @@ data "aws_subnet" "subnet" {
   id = var.subnet_id
 }
 
-resource "aws_security_group" "instance" {
-  count  = var.egress_everywhere ? 1 : 0
-  name   = "${var.name_prefix}-instance"
-  vpc_id = data.aws_subnet.subnet.vpc_id
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
 data "aws_ami" "amazon2" {
   most_recent = true
 
@@ -24,24 +10,6 @@ data "aws_ami" "amazon2" {
     values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
   owners = ["amazon"]
-}
-
-resource "aws_instance" "instance" {
-  ami           = data.aws_ami.amazon2.id
-  instance_type = var.instance_type
-
-  user_data              = data.template_cloudinit_config.instance_userdata.rendered
-  iam_instance_profile   = aws_iam_instance_profile.instance.id
-  subnet_id              = data.aws_subnet.subnet.id
-  vpc_security_group_ids = concat(aws_security_group.instance.*.id, var.extra_security_groups)
-
-  root_block_device {
-    volume_size = var.root_volume_size_gb
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-instance"
-  }
 }
 
 data "template_cloudinit_config" "instance_userdata" {
@@ -54,9 +22,18 @@ data "template_cloudinit_config" "instance_userdata" {
   }
 }
 
-resource "aws_iam_instance_profile" "instance" {
-  name = "${var.name_prefix}-instance"
-  role = aws_iam_role.instance.name
+resource "aws_security_group" "instance" {
+  count  = var.egress_everywhere ? 1 : 0
+  name   = "${var.name_prefix}-instance"
+  vpc_id = data.aws_subnet.subnet.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_iam_role" "instance" {
@@ -86,4 +63,27 @@ resource "aws_iam_role_policy_attachment" "extra-attach" {
   for_each   = toset(var.extra_iam_policies)
   role       = aws_iam_role.instance.name
   policy_arn = each.key
+}
+
+resource "aws_iam_instance_profile" "instance" {
+  name = "${var.name_prefix}-instance"
+  role = aws_iam_role.instance.name
+}
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.amazon2.id
+  instance_type = var.instance_type
+
+  user_data              = data.template_cloudinit_config.instance_userdata.rendered
+  iam_instance_profile   = aws_iam_instance_profile.instance.id
+  subnet_id              = data.aws_subnet.subnet.id
+  vpc_security_group_ids = concat(aws_security_group.instance.*.id, var.extra_security_groups)
+
+  root_block_device {
+    volume_size = var.root_volume_size_gb
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-instance"
+  }
 }
